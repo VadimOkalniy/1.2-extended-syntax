08e51c780e00b219d96bb525f02306eebdd7dc74
Vadim Okalniy <vadimokalniy16@gmail.com>
2020-11-25 15:43:52 +0200

    Решение

--- task.js
+++ task.js
@@ -1,25 +1,20 @@
 function getResult(a, b, c) {
     // код для задачи №1 писать здесь
     // return x;
-    let x1
-    let x2
     let d = Math.pow(b, 2) - 4 * a * c;
-    let arr = [x1, x2];
-    if (d < 0) {
-
-    }
-    else if (d == 0) {
-        x1 = (-b + Math.sqrt(d)) / 2 * a;
-        arr[0] = x1;
-        arr[1] = null;
-
-    }
+    let arr = [];
+  for (i=0; i<=1; i++)
+  { if (d == 0) {
+       arr[i] = (-b + Math.sqrt(d)) / 2 * a
+        return arr;
+  }
     else if (d > 0) {
-        arr[0] = (-b + Math.sqrt(d)) / 2 * a;
-        arr[1] = (-b - Math.sqrt(d)) / 2 * a;
-
+        arr[i] = (-b + Math.sqrt(d)) / 2 * a;
+        arr[i+1] = (-b - Math.sqrt(d)) / 2 * a;
+ return arr;
     }
-    return arr;
+     
+}
 }
 
 function getAverageMark(marks) {
@@ -35,28 +30,24 @@ function getAverageMark(marks) {
     if (marks.length == 0) {
         return 0;
     }
-    else {
+    if(marks.lenght>5){
+        console.log("В массиве больше 5 оценок")
+        m = marks.splice(5)
+    }
+     
         for (let i = 0; i < marks.length; i++) {
-
-            count = count + 1;
-            if (count > 5) {
-                m = marks.splice(5)
-                console.log("В массиве больше 5 оценок")
-                return sum = sum / (count - 1);
-            }
+            
+            
             sum = sum + marks[i];
         }
-        sum = sum / count
-        return sum;
+
+        
+        return sum=sum /marks.lenght;
         //  return s;
-    }
+    
 
 }
 
 
 
 
-function askDrink(name, dateOfBirthday) {
-    // код для задачи №3 писать здесь
-    // return result;
-}
